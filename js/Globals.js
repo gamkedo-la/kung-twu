91e1f36ccba8983f84d336c2bae3cbe590843a72
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2019-09-27 13:13:14 -0700

    Fixed KeyCodes

--- js/Globals.js
+++ js/Globals.js
@@ -191,11 +191,18 @@ const ACTION_TYPE = {
  * Input Codes representing Keyboard keys. Used when querying input
  */
 const KeyCode = {
-	BACKSPACE: 8, TAB: 9, ENTER: 1, ESCAPE: 2, SPACE: 3, LEFT: 3, UP: 3, RIGHT: 3, DOWN: 4,
-	DIGIT_0: 4, DIGIT_1: 4, DIGIT_2: 5, DIGIT_3: 51, DIGIT_4: 52, DIGIT_5: 53, DIGIT_6: 54, DIGIT_7: 55, DIGIT_8: 56, DIGIT_9: 57,
+	BACKSPACE: 8, TAB: 9, ENTER: 13, ESCAPE: 27, SPACE: 32, LEFT: 37, UP: 38, RIGHT: 39, DOWN: 40,
+	DIGIT_0: 48, DIGIT_1: 49, DIGIT_2: 50, DIGIT_3: 51, DIGIT_4: 52, DIGIT_5: 53, DIGIT_6: 54, DIGIT_7: 55, DIGIT_8: 56, DIGIT_9: 57,
 	A: 65, B: 66, C: 67, D: 68, E: 69, F: 70, G: 71, H: 72, I: 73, J: 74, K: 75, L: 76, M: 77, N: 78, O: 79, P: 80, Q: 81,   R: 82, S: 83, T: 84, U: 85, V: 86, W: 87, X: 88, Y: 89, Z: 90,
 	PLUS: 187, MINUS: 189, TILDE: 192
 }; Object.freeze(KeyCode);
+
+// const KeyCode = {
+// 	BACKSPACE: 8, TAB: 9, ENTER: 1, ESCAPE: 2, SPACE: 3, LEFT: 3, UP: 3, RIGHT: 3, DOWN: 4,
+// 	DIGIT_0: 4, DIGIT_1: 4, DIGIT_2: 5, DIGIT_3: 51, DIGIT_4: 52, DIGIT_5: 53, DIGIT_6: 54, DIGIT_7: 55, DIGIT_8: 56, DIGIT_9: 57,
+// 	A: 65, B: 66, C: 67, D: 68, E: 69, F: 70, G: 71, H: 72, I: 73, J: 74, K: 75, L: 76, M: 77, N: 78, O: 79, P: 80, Q: 81,   R: 82, S: 83, T: 84, U: 85, V: 86, W: 87, X: 88, Y: 89, Z: 90,
+// 	PLUS: 187, MINUS: 189, TILDE: 192
+// }; Object.freeze(KeyCode);
 
 /**
  * Input Codes representing Mouse buttons. Used when querying input
