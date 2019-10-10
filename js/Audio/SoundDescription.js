0f6adfc8f6ad0adedb437c27658b1e15a6652a13
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2019-10-09 22:57:40 -0700

    Adapted SoundInstance to behave as SoundOverlapsClass to allow multiple instances, but it's getting to be a huge class. Thinking of making SoundOverlaps a manager of SoundInstances instead.

--- js/Audio/SoundDescription.js
+++ js/Audio/SoundDescription.js
@@ -2,13 +2,17 @@
  * Class that will function as a mold for creating new sound objects. 
  * These will be instantiated at load-time to be stored and used by an audio manager/engine class 
  * to easily create sounds on the fly with their intended levels/properties.
+ * @param {SoundEngine} soundEngine SoundEngine instance
  * @param {string} key The key which to reference this SoundDescription by.
  * @param {string} filepath The path to the sound resource
  * @param {number} baseVolume The intended base volume of the sound
  * @param {string} audioBus The key of the 'bus' to affect the sound through, which is a value within AudioBus enum object. Audio bus will affect gain only.
  * @param {boolean} isLoop (true) will loop, (false) will not loop
+ * @param {number} maxInstances The max number of SoundInstances playable at a given time. Behavior will stop the oldest instance and start a new one.
+ * @param {number} fadeOutTime The number of seconds of fading out a sound when calling "stop".
  */
-function SoundDescription(key, filepath, baseVolume, audioBus, isLoop) {
+function SoundDescription(soundEngine, key, filepath, baseVolume, audioBus, isLoop, maxInstances, fadeOutTime = 0) {
+	const _engine = soundEngine;
 	/**
 	 * The base volume level for the track that can be easily adjusted
 	 * Range (0 - 1)
@@ -39,7 +43,18 @@ function SoundDescription(key, filepath, baseVolume, audioBus, isLoop) {
 	 */
 	this.isLoop = isLoop;
 
+	/**
+	 * The number of audio files that can play at a given time
+	 * @type {number}
+	 */
+	this.maxInstances = maxInstances;
+
+	/**
+	 * The number of seconds of fading out a sound when calling "stop".
+	 */
+	this.fadeOutTime = fadeOutTime;
+
 	this.createInstance = function() {
-		return new SoundInstance(this.filepath, this.baseVolume, 1, this.audioBus, this.isLoop);
+		return new SoundInstance(_engine, this.filepath, this.baseVolume, 1, this.audioBus, this.isLoop, this.maxInstances, this.fadeOutTime);
 	};
 }
--- js/Audio/SoundInstance.js
+++ js/Audio/SoundInstance.js
@@ -2,13 +2,15 @@
 /**
  * A sound instance. 
  * @param {SoundEngine} soundEngine Sound engine instance this instance belongs to
- * @param {string} filepath 
- * @param {number} baseVolume
- * @param {number} startingVol
+ * @param {string} filepath Filepath of the audio file
+ * @param {number} baseVolume Base volume
+ * @param {number} startingVol Starting volume. Range 0-1
  * @param {string} audioBus A value within the AudioBus 'enum' object
  * @param {boolean} isLoop Sets if this is a loop or not
+ * @param {number} maxInstances The max number of inner instances that can play at one time
+ * @param {number} fadeOutTime The number of seconds of fading out a sound when calling "stop".
  */
-function SoundInstance(soundEngine, filepath, baseVolume, startingVol, audioBus, isLoop) {
+function SoundInstance(soundEngine, filepath, baseVolume, startingVol, audioBus, isLoop, maxInstances, fadeOutTime) {
 
 	// ========= Initialization =============== //
 	/** 
@@ -18,28 +20,66 @@ function SoundInstance(soundEngine, filepath, baseVolume, startingVol, audioBus,
 	let _volume = clamp(startingVol, 0, 1);
 	/** @type {SoundEngine} */
 	const _engine = soundEngine;
-	/** @type {HTMLAudioElement} */
-	const _element = new Audio(filepath);
+	/** @type {HTMLAudioElement[]} */
+	const _elements = [];
+	for (let i = 0; i < Math.max(1, maxInstances); i++) { // (make sure maxInstnaces is at least 1)
+		_elements.push(new Audio(filepath));
+	}
+	let _playIndex = 0; // current index of elements to play next
 	const _baseVolume = baseVolume; // inject baseVolume for volume calculation
 
 	/** @type {string} A value within AudioBus enum object */
 	let _audioBus = audioBus || AudioBus.NONE;
-
-	_setLooping(isLoop); // set looping to the inner html element
+	_setLooping(isLoop); // set looping to the inner html elements
 
 	/** @type {NodeJS.Timeout} */
 	let _fade = null;
+
+	/** 
+	 * The number of seconds of fade when stopping. May be bypassed to immediate stop by passing false in stop.
+	 * @type {number} 
+	 */
+	let _fadeOutTime = fadeOutTime;
+
+
+	// ============================== //
 	// ======== Public API ========== //
+	// ============================== //
+
+	/**
+	 * Start playing the next free inner sound instance.
+	 * If all are currently playing, the oldest one will be 'stolen'
+	 */
 	this.play = function() {
-		_element.play();
+		const e = _elements[_playIndex];
+		if (e.paused) {
+			// Next free instance is free to play: play it
+			e.play();
+		} else {
+			// Next free instance is actually still busy: pause, reset, and play it
+			e.pause();
+			e.currentTime = 0;
+			e.play();
+		}
+		// Increase the playindex by 1, and wrap around to 0 if outside the bounds
+		_playIndex = (_playIndex + 1) % _elements.length;
 	};
 
-	this.getLooping = function() {
-		return _element.loop;
-	};
-
-	this.getIsFading = function() {
-		(_fade) ? true : false;
+	/**
+	 * Stops all inner instances of the SoundInstance
+	 * @param fadeOut Default = true. Fades out on true, immediate stop on false
+	 * @param {number?} overrideFadeTimeValue Optional parameter. When set with a number, it will override the instance's preset fadeOutTime value.
+	 */
+	this.stop = function(fadeOut = true, overrideFadeTimeValue) {
+		if (fadeOut) {
+			_fadeTo(0, overrideFadeTimeValue ||  _fadeOutTime, (e)=> {
+				e.setPaused(true);
+			});
+		} else {
+			_elements.forEach((e) => {
+				e.pause();
+			});
+		}
 	};
 
 	/**
@@ -48,58 +88,53 @@ function SoundInstance(soundEngine, filepath, baseVolume, startingVol, audioBus,
 	 * @param {number} seconds fade time in seconds
 	 * @param {(inst: SoundInstance)=>void}onTargetReached The callback to call when this fade is complete, please make sure to bind 'this' ahead of time
 	 */
-	this.fadeTo = function(targetVol, seconds, onTargetReached) {
-		this.cancelFade();
-		const fade = _fadeFromTo(this, _volume, targetVol, seconds, (sound) => {
-			onTargetReached(sound);
-			_fade = null;
-		});
-		if (fade) {
-			_fade = fade;
-		}
+	this.fadeTo = _fadeTo; // open private function for public use also
+
+	/**
+	 * Gets whether or not the track is currently fading
+	 */
+	this.getIsFading = function() {
+		(_fade === null) ? false : true; // null is the default "not fading" value
 	};
 
 	/**
 	 * Cancels the current fade if there is one
 	 */
 	this.cancelFade = function() {
-		if (_fade) {
+		if (_fade != null) {
+			// There is a fade to cancel: clear it, set it to the default val null
 			clearInterval(_fade);
 			_fade = null;
 		}
 	};
 
-	this.setLooping = _setLooping;
+	/**
+	 * Sets the looping status of each internal sound instance mid-game.
+	 * Suggestion: Loops are best faded out. If the preset fadeout length is 0, you can override it in stop
+	 */
+	this.setLooping = _setLooping; // open this function for public use also
 	function _setLooping(isLoop) {
-		if (isLoop === true) { // using triple equals to make sure it's a boolean
-			_element.loop = true;
-		} else if (isLoop === false) {
-			_element.loop = false;
-		} else {
-			console.log("SoundInstance setLoop was passed an invalid parameter! Must be a boolean");
-		}
+		_elements.forEach((e) => {
+			e.loop = isLoop;
+		});
 	}
 
-
-	/**
-	 * Set the paused status of the SoundInstance
-	 */
-	this.setPaused = function(paused) {
-		if (paused === true) { // Using triple equals to make sure it is a boolean
-			_element.pause();
-		} else if (paused === false) {
-			_element.play();
-		} else {
-			console.log("SoundInstance setPaused was passed an invalid parameter! Must be a boolean");
-		}
+	this.getLooping = function() {
+		return _elements[0].loop;
 	};
 
-	this.setVolume = _setVolume;
+	/**
+	 * Sets the volume of the track or simply updates it if no volume is passed.
+	 * Affects all inner sound instances.
+	 * @param {number} vol 
+	 */
+	this.setVolume = _setVolume; // Open this function for public use also
 
 	this.getVolume = () => _volume;
 
 	/**
 	 * Sets this instance's audio bus to another one
+	 * Affects all inner sound instances
 	 * @param {string} audioBus A value from the global AudioBus 'enum' object
 	 */
 	this.setBus = function(audioBus) {
@@ -115,24 +150,84 @@ function SoundInstance(soundEngine, filepath, baseVolume, startingVol, audioBus,
 	};
 
 	/**
-	 * Gets whether or not this SoundInstance is paused.
+	 * Set the paused status of every inner sound instance
+	 * @param {boolean} paused Paused status to set
+	 */
+	this.setPausedAll = function(paused) {
+		if (paused === true) { // Using triple equals to make sure it is a boolean
+			_elements.forEach((e) => {
+				e.pause();
+			});
+		} else if (paused === false) {
+			// We've unpaused the instance: Set all inner instances that were playing to start playing again
+			_elements.forEach((e) => {
+				if (e.currentTime > 0) { // Detecting was playing by the currentTime being beyond the starting point
+					e.play();
+				}
+			});
+		} else {
+			console.log("SoundInstance setPaused was passed an invalid parameter! Must be a boolean");
+		}
+	};
+
+	/**
+	 * Set the paused status of the last played inner sound instance
+	 * @param {boolean} paused Paused status to set
+	 */
+	this.setPaused = function(paused) {
+		if (paused === true) {
+			_getLastPlayed().pause();
+		} else {
+			_getLastPlayed().play();
+		}
+	};
+
+	/**
+	 * Gets whether or not the latest inner instance is paused.
 	 * @returns {boolean}
 	 */
 	this.getPaused = function() {
-		return _element.paused;
+		return _elements[_getLastPlayedIndex()].paused;
 	};
 
+	/**
+	 * Sets the track time of the last played inner instance. If in the middle of playing, will continue from that point on, if paused, it will play at that position when played or unpaused.
+	 * Caveat: if this current inner instance is paused and if this.setPause is set to false later, this instance will play at the set track time.
+	 */
 	this.setTrackTime = function(time) {
-		_element.currentTime = time;
+		const e = _getLastPlayed();
+		e.currentTime = time;
 	};
 
+	/**
+	 * Gets the track time of the last played inner instance.
+	 */
 	this.getTrackTime = function() {
-		return _element.currentTime;
+		return _getLastPlayed().currentTime;
 	};
 
-	// Private helpers
+
+	// ====================================== //
+	// ======== Private Helpers ============= //
+	// ====================================== //
+
+	/**
+	 * Helper that gets the last played inner instance
+	 */
+	function _getLastPlayed() {
+		return _elements[_getLastPlayedIndex()];
+	}
+	/**
+	 * Helper that gets the last played inner instance index
+	 */
+	function _getLastPlayedIndex() {
+		// Quick hack to wrap around to last index if below 0 (since we don't have a true modulus function)
+		return (_playIndex - 1 < 0) ? _elements.length - 1 : _playIndex - 1; 
+	}
+
 	/**
 	 * Sets the volume of the track or simply updates it if no volume is passed.
+	 * Affects all inner sound instances.
 	 * @param {number} vol 
 	 */
 	function _setVolume(vol) {
@@ -141,8 +236,10 @@ function SoundInstance(soundEngine, filepath, baseVolume, startingVol, audioBus,
 		if (vol != undefined && vol != null && typeof vol === "number") {
 			_volume = clamp(vol, 0, 1);
 		}
-		// Set the internal volume
-		_element.volume = _calculateTrackVolume(_baseVolume, _audioBus, _volume);
+		// Calculate and set the volume param of every internal instance
+		_elements.forEach((e) => {
+			e.volume = _calculateTrackVolume(_baseVolume, _audioBus, _volume);
+		});
 	} 
 
 	/**
@@ -151,19 +248,9 @@ function SoundInstance(soundEngine, filepath, baseVolume, startingVol, audioBus,
 	 * @param {string} bus 
 	 * @param {number} volume 
 	 */
-	function _calculateTrackVolume(baseVolume, bus, volume) {
-		// Make sure these values are as expected
-		// Remove later for performance
-		Debug.isValid(baseVolume, "number");
-		Debug.isValid(bus, "string");
-		Debug.isValid(volume, "number");
-		Debug.isValid(musicVolume, "number");
-		Debug.isValid(effectsVolume, "number");
-		Debug.isValid(masterVolume, "number");
-		Debug.isValid(isMuted, "boolean");
-	
+	function _calculateTrackVolume(baseVolume, bus, volume) {	
 		// Calculate volume here. User can omit or set volume to null to not alter the value, but rather update the volume based on bus.
-		if (isMuted) {
+		if (_engine.isMuted) {
 			// avoid having to calculate volume if muted
 			return 0;
 		} else {
@@ -176,6 +263,25 @@ function SoundInstance(soundEngine, filepath, baseVolume, startingVol, audioBus,
 			const _base = baseVolume;
 			const _master = masterVolume;
 			return Math.pow(_vol * _base * _bus * _master, 2);
+		}
+	}
+
+	/**
+	 * Fade this sound to a target volume
+	 * @param {number} targetVol value between 0-1
+	 * @param {number} seconds fade time in seconds
+	 * @param {(inst: SoundInstance)=>void}onTargetReached The callback to call when this fade is complete, please make sure to bind 'this' ahead of time
+	 */
+	function _fadeTo(targetVol, seconds, onTargetReached) {
+		this.cancelFade(); // cancel current fade to prevent jumpy multi-fade bug
+		const fade = _fadeFromTo(this, _volume, targetVol, seconds, (sound) => {
+			// Target volume has been reached: Reset fade value and call callback.
+			_fade = null;
+			onTargetReached(sound);	
+		});
+		// If we have a fade returned, set the fade property to that value
+		if (fade != null && fade != undefined) {
+			_fade = fade;
 		}
 	}
 
