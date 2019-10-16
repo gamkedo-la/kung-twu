8816f98c95d357f0f0cd0b9848771b91f6cbcf11
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2019-10-16 02:10:13 -0700

    Added all current sound info into a config-like file. Will balance them soon.

---
+++ js/Audio/SoundGlobals.js
@@ -0,0 +1,228 @@
+/**
+ * Keys to access sounds with
+ */
+const Sounds = {
+	SFX_1UP: "SFX_1UP",
+	SFX_BoneCrack: "SFX_BoneCrack",
+	SFX_EnemyHit: "SFX_EnemyHit",
+	SFX_Footsteps: "SFX_Footsteps",
+	SFX_LowHealth: "SFX_LowHealth",
+	SFX_LowPain: "SFX_LowPain",
+	SFX_MenuNav: "SFX_MenuNav",
+	SFX_MenuSelect: "SFX_MenuSelect",
+	SFX_PauseLow: "SFX_PauseLow",
+	SFX_PlayerBlock: "SFX_PlayerBlock",
+	SFX_PlayerFail: "SFX_PlayerFail",
+	SFX_PlayerHit: "SFX_PlayerHit",
+	SFX_PlayerJump: "SFX_PlayerJump",
+	SFX_PlayerKick: "SFX_PlayerKick",
+	SFX_PlayerPunch: "SFX_PlayerPunch",
+	SFX_ResumeLow: "SFX_ResumeLow",
+	SFX_Swish_01: "SFX_Swish_01",
+	SFX_Swish_02: "SFX_Swish_02",
+	BGM_Boss: "BGM_Boss",
+	BGM_GamePlay: "BGM_GamePlay",
+	BGM_Title: "BGM_Title"
+}; Object.freeze(Sounds);
+// ========================
+
+/**
+ * SoundSprite Configuration 
+ */
+const soundSpriteConfigs = [
+	{
+		key: Sounds.SFX_1UP,
+		filepath: "./audio/1_UP",
+		baseVolume: .15,
+		audioBus: AudioBus.SFX,
+		isLoop: false,
+		maxInstances: 2,
+		fadeOutTime: 0
+	},
+	{
+		key: Sounds.SFX_BoneCrack,
+		filepath: "./audio/boneCrack",
+		baseVolume: .5,
+		audioBus: AudioBus.SFX,
+		isLoop: false,
+		maxInstances: 2,
+		fadeOutTime: 0
+	},
+	{
+		key: Sounds.SFX_EnemyHit,
+		filepath: "./audio/BasicEnemyHit",
+		baseVolume: .5,
+		audioBus: AudioBus.SFX,
+		isLoop: false,
+		maxInstances: 2,
+		fadeOutTime: 0
+	},
+	{
+		key: Sounds.SFX_Footsteps,
+		filepath: "./audio/Footsteps",
+		baseVolume: .4,
+		audioBus: AudioBus.SFX,
+		isLoop: false,
+		maxInstances: 2,
+		fadeOutTime: 0
+	},
+	{
+		key: Sounds.SFX_LowHealth,
+		filepath: "./audio/lowHealth",
+		baseVolume: .4,
+		audioBus: AudioBus.SFX,
+		isLoop: false,
+		maxInstances: 2,
+		fadeOutTime: 0
+	},
+	{
+		key: Sounds.SFX_LowPain,
+		filepath: "./audio/lowPain",
+		baseVolume: .4,
+		audioBus: AudioBus.SFX,
+		isLoop: false,
+		maxInstances: 2,
+		fadeOutTime: 0
+	},
+	{
+		key: Sounds.SFX_MenuNav,
+		filepath: "./audio/MenuNavigation",
+		baseVolume: .8,
+		audioBus: AudioBus.SFX,
+		isLoop: false,
+		maxInstances: 2,
+		fadeOutTime: 0
+	},
+	{
+		key: Sounds.SFX_MenuSelect,
+		filepath: "./audio/MenuSelection",
+		baseVolume: .4,
+		audioBus: AudioBus.SFX,
+		isLoop: false,
+		maxInstances: 2,
+		fadeOutTime: 0
+	},
+	{
+		key: Sounds.SFX_PauseLow,
+		filepath: "./audio/PauseSoundLow",
+		baseVolume: .4,
+		audioBus: AudioBus.SFX,
+		isLoop: false,
+		maxInstances: 2,
+		fadeOutTime: 0
+	},
+	{
+		key: Sounds.SFX_PlayerBlock,
+		filepath: "./audio/PlayerBlocking",
+		baseVolume: .4,
+		audioBus: AudioBus.SFX,
+		isLoop: false,
+		maxInstances: 2,
+		fadeOutTime: 0
+	},
+	{
+		key: Sounds.SFX_PlayerFail,
+		filepath: "./audio/PlayerFailed",
+		baseVolume: .4,
+		audioBus: AudioBus.SFX,
+		isLoop: false,
+		maxInstances: 2,
+		fadeOutTime: 0
+	},
+	{
+		key: Sounds.SFX_PlayerHit,
+		filepath: "./audio/PlayerHit",
+		baseVolume: .4,
+		audioBus: AudioBus.SFX,
+		isLoop: false,
+		maxInstances: 2,
+		fadeOutTime: 0
+	},
+	{
+		key: Sounds.SFX_PlayerJump,
+		filepath: "./audio/PlayerJump",
+		baseVolume: .4,
+		audioBus: AudioBus.SFX,
+		isLoop: false,
+		maxInstances: 2,
+		fadeOutTime: 0
+	},
+	{
+		key: Sounds.SFX_PlayerKick,
+		filepath: "./audio/PlayerKick",
+		baseVolume: .4,
+		audioBus: AudioBus.SFX,
+		isLoop: false,
+		maxInstances: 2,
+		fadeOutTime: 0
+	},
+	{
+		key: Sounds.SFX_PlayerPunch,
+		filepath: "./audio/PlayerPunch",
+		baseVolume: .4,
+		audioBus: AudioBus.SFX,
+		isLoop: false,
+		maxInstances: 2,
+		fadeOutTime: 0
+	},
+	{
+		key: Sounds.SFX_ResumeLow,
+		filepath: "./audio/ResumeSoundLow",
+		baseVolume: .4,
+		audioBus: AudioBus.SFX,
+		isLoop: false,
+		maxInstances: 2,
+		fadeOutTime: 0
+	},
+	{
+		key: Sounds.SFX_Swish_01,
+		filepath: "./audio/SwishTrim1",
+		baseVolume: .4,
+		audioBus: AudioBus.SFX,
+		isLoop: false,
+		maxInstances: 2,
+		fadeOutTime: 0
+	},
+	{
+		key: Sounds.SFX_Swish_02,
+		filepath: "./audio/SwishTrim2",
+		baseVolume: .4,
+		audioBus: AudioBus.SFX,
+		isLoop: false,
+		maxInstances: 2,
+		fadeOutTime: 0
+	},
+	{
+		key: Sounds.BGM_Title,
+		filepath: "./audio/gameplayMusicV2",
+		baseVolume: .5,
+		audioBus: AudioBus.MUSIC,
+		isLoop: true,
+		maxInstances: 2,
+		fadeOutTime: 1.5
+	},		
+	{
+		key: Sounds.BGM_GamePlay,
+		filepath: "./audio/Dragon Pulse_v2",
+		baseVolume: .25,
+		audioBus: AudioBus.MUSIC,
+		isLoop: true,
+		maxInstances: 2,
+		fadeOutTime: 1.5
+	},
+	{
+		key: Sounds.BGM_Boss,
+		filepath: "./audio/AWarriorsResolution(loop)",
+		baseVolume: .25,
+		audioBus: AudioBus.MUSIC,
+		isLoop: true,
+		maxInstances: 2,
+		fadeOutTime: 1.5
+	}
+];
+
+// // Create the SoundManager
+// const sounds = new SoundManager();
+
+// // Add SoundSprites
+// sounds.addSounds(soundSpriteConfigs);
