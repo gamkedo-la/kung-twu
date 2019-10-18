aad1c3f6a764767e4562e7f4eeb4a9a7cbe656ff
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2019-10-17 17:32:30 -0700

    Reduced memory footprint of each SoundInstance by using prototype chain/class syntax.

--- js/Audio/SoundInstance.js
+++ js/Audio/SoundInstance.js
@@ -3,146 +3,175 @@
  * @param {SoundSprite} soundSprite
  * @param {string} filename Filename + extension. Please get the compatible extension from AudioEngine.getSoundFormat
  */
-function SoundInstance(soundSprite, filename) {
-	this.sprite = soundSprite;
+class SoundInstance {
+	/** @type SoundSprite */
+	sprite;
+
+	/**
+	 * Sound's filename including extension
+	 */
+	filename;
 
 	/**
 	 * The inner HTMLAudioElement
 	 * @type {HTMLAudioElement}
 	 */
-	const _element = new Audio(filename);
+	_element;
+
+	/**
+	 * The inner HTMLAudioElement
+	 */
+	get element() { return this._element; }
 
 	/**
 	 * Current handles to a NodeJS.Timeout
 	 * @type {number[]}
 	 */
-	const _fades = [];
-
-	this.setFade = function(fade) {
-		if (_fades.indexOf(fade) === -1) {
-			_fades.push(fade);
-		}
-	};
-
-	/**
-	 * Cancels a specific fade on this SoundInstance. (Currently not used, but may become necessary while bug testing)
-	 */
-	this.cancelFade = function(fade) {
-		const index = _fades.indexOf(fade);
-		if (index !== -1) {
-			_fades.splice(index, 1);
-		}
-	};
-
-	/**
-	 * Sets the looping status of this instance
-	 * @param {boolean} isLoop
-	 */
-	this.setLoop = function(isLoop) {
-		_element.loop = isLoop;
-	};
-
-	/**
-	 * Sets the looping status of this instance
-	 * @param {boolean} isLoop
-	 */
-	this.getLoop = function() {
-		return _element.loop;
-	};
+	_fades = [];
 
 	/**
 	 * The 'dial' the api turns for setting volume. Under the hood it calculates bus volume values.
 	 */
-	let _volume = 1;
+	_volume = 1;
+
+	constructor(soundSprite, filename) {
+		this.sprite = soundSprite;
+		this._element = new Audio(filename);
+		this.filename = filename;
+	}
 
 	/**
-	 * The inner HTMLAudioElement
+	 * Adds a fade to the SoundInstance. Used by fading functions, not to be handled directly.
+	 * @param {NodeJS.Timeout} fade 
 	 */
-	this.element = _element;
+	setFade(fade) {
+		const _fades = this._fades;
+		if (_fades.indexOf(fade) === -1) {
+			_fades.push(fade);
+		}
+	}
 
 	/**
-	 * Sound's filename including extension
+	 * Cancels a specific fade on this SoundInstance. (Currently not used, but may become necessary while bug testing)
 	 */
-	this.filename = filename;
+	cancelFade(fade) {
+		const _fades = this._fades;
+		const index = _fades.indexOf(fade);
+		if (index !== -1) {
+			_fades.splice(index, 1);
+		}
+	}
 
-	this.getIsFading = function() {
-		(_fades.length > 0) ? true : false;
-	};
+	/**
+	 * Sets the looping status of this instance
+	 * @param {boolean} isLoop
+	 */
+	setLoop(isLoop) {
+		this._element.loop = isLoop;
+	}
 
+	/**
+	 * Sets the looping status of this instance
+	 * @param {boolean} isLoop
+	 */
+	getLoop() {
+		return this._element.loop;
+	}
+
+
+	/**
+	 * Gets the current fading status of the sound instance
+	 */
+	getIsFading() {
+		(this._fades.length > 0) ? true : false;
+	}
 	/**
 	 * Cancels the current fade if there are any
 	 */
-	this.cancelFadeAll = function() {
+	cancelFadeAll() {
+		const _fades = this._fades;
 		while (_fades.length > 0) {
 			const fade = _fades.shift();
 			clearInterval(fade);
 		}
-	};
+	}
 
-	this.getIsPlaying = function() {
-		return !_element.paused;
-	};
+	getIsPlaying() {
+		return !this._element.paused;
+	}
 
-	this.getIsPaused = function() {
-		return _element.paused;
-	};
+	getIsPaused() {
+		return this._element.paused;
+	}
 
-	this.getVolume = function() {
-		return _volume;
-	};
+	getVolume() {
+		return this._volume;
+	}
 
 	/**
 	 * Make sure that the sound sprite updates volume after a call to this fn
 	 */
-	this.setVolume = function(vol) {
+	setVolume(vol) {
 		if (typeof vol === "number" && !Number.isNaN(vol)) {
-			_volume = clamp(vol, 0, 1);
-		} else {
+			this._volume = clamp(vol, 0, 1);
+		}
+		else {
 			throw new Error("Trying to set volume to a value that is not a number!");
 		}
-	};
+	}
 
-	this._setInnerVolume = function(vol) {
-		_element.volume = clamp(vol, 0, 1);
-	};
+	/**
+	 * Sets the inner volume directly. Used by the SoundEngine and typically should not be used directly.
+	 * @param {number} vol 
+	 */
+	_setInnerVolume(vol) {
+		this._element.volume = clamp(vol, 0, 1);
+	}
 
 	/**
 	 * Pauses the audio
 	 */
-	this.pause = function() {
-		_element.pause();
-	};
+	pause() {
+		this._element.pause();
+	}
 
 	/**
 	 * Start playing the audio
 	 */
-	this.play = function() {
-		_element.play()
-			.catch(() => {
-				console.log("For your enjoyment of the web, the game has prevented sounds from playing.\nPlease click or tap anywhere on the screen to allow sound.");
+	play() {
+		this._element.play()
+			.catch((err) => {
+				if (err === "DomException") {
+					// Console message to display when sounds fire before the user has the opportunity to interact with the page.
+					console.log("For your enjoyment of the web, the game has prevented sounds from playing.\nPlease click or tap anywhere on the screen to allow sound.");
+				} else {
+					// Generic error
+					console.log("Audio Error: ", err);
+				}
+				
 			});
-	};
+	}
 
 	/**
 	 * Set the current track time
 	 * @param {number} seconds The number of seconds
 	 */
-	this.setCurrentTime = function(seconds) {
-		_element.currentTime = seconds;
-	};
+	setCurrentTime(seconds) {
+		this._element.currentTime = seconds;
+	}
 
 	/**
 	 * Get the current track time
 	 */
-	this.getCurrentTime = function() {
-		return _element.currentTime;
-	};
+	getCurrentTime() {
+		return this._element.currentTime;
+	}
 
 	/**
 	 * Checks if instance of a SoundSprite
 	 * @param {SoundSprite} soundSprite
 	 */
-	this.isInstanceOf = function(soundSprite) {
+	isInstanceOf(soundSprite) {
 		return (soundSprite === this.sprite);
-	};
+	}
 }
